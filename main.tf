18d3715c5fcbbe37c0da0609dd31f0f8496bb6e3
Sourodip Nath <102436019+Sourodip2001@users.noreply.github.com>
2026-09-18 23:38:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "vpc" {
-    source = "./module"
-    cidr_block = var.cidr_block
-    public_subnet_cidrs = var.public_subnet_cidrs
-    private_subnet_cidrs = var.private_subnet_cidrs
-    availability_zones = var.availability_zones
-    availability_zone = var.availability_zone
-    enable_dns_hostnames = var.enable_dns_hostnames
-    enable_dns_support = var.enable_dns_support
+  source = "./module"
+
+  cidr_block           = var.cidr_block
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  availability_zone    = var.availability_zone
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
 }
