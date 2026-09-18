68d28b167d574d22e750d722b7e8b49e30d34c8b
Sourodip Nath <102436019+Sourodip2001@users.noreply.github.com>
2026-09-19 00:01:28 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
     count = length(var.public_subnet_cidrs)
     vpc_id = aws_vpc.main.id 
-    availability_zone = var.availability_zone[count.index]
+    availability_zone = var.availability_zones[count.index]
     cidr_block = var.public_subnet_cidrs[count.index]
     map_public_ip_on_launch = true
 }
@@ -15,7 +15,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
     count = length(var.private_subnet_cidrs)
     vpc_id = aws_vpc.main.id 
-    availability_zone = var.availability_zone[count.index]
+    availability_zone = var.availability_zones[count.index]
     cidr_block = var.private_subnet_cidrs[count.index]
     map_public_ip_on_launch = false
 }
